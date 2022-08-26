cf36e24caf5a8e2b22d91b33b38c01c9c01015bf
Alexander <raskalovalexander@gmail.com>
2022-08-26 20:42:48 +0300

    refactor: project structure

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,9 +1,7 @@
 import { StyleSheet, View, StatusBar, ImageBackground } from 'react-native';
 import { useForm } from 'react-hook-form';
-import { Button } from '../components/UI/Button';
-import { Input } from '../components/UI/Input';
-import { Form } from '../components/UI/Form';
 import background from '../assets/images/background.png';
+import { Button, Form, Input } from '../components/UI';
 
 const loginRules = {
   required: 'Логин не может быть пустым',
