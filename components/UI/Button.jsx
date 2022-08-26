bd50bbdf845fe72f4e56124369533e830dfb1a65
Alexander <raskalovalexander@gmail.com>
2022-08-26 20:33:29 +0300

    feat: UI Button Component

---
+++ components/UI/Button.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Pressable, Text, StyleSheet } from 'react-native';
+
+export const Button = ({ isValid, onSubmit }) => {
+  return (
+    <Pressable
+      style={[styles.button, !isValid ? '' : styles.button_active]}
+      onPress={onSubmit}
+      disabled={!isValid}
+    >
+      <Text style={styles.button__text}>Войти</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#41522E',
+    width: '100%',
+    borderRadius: 4,
+    height: 50,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 12,
+  },
+  button_active: {
+    backgroundColor: '#BE8022',
+  },
+  button__text: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 16,
+    lineHeight: 20,
+  },
+});
